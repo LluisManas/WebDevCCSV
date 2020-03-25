f5926c689a07434e273b3a9a3431e4e1a6934e5c
LluisManas <manastorra@gmail.com>
2020-03-25 16:24:56 +0100

    1,2,3,4

--- src/App.js
+++ src/App.js
@@ -1,72 +1,120 @@
-import React from 'react';
-import result from './data.json'
-import InputField from './InputField'
-import SearchButton from './SearchButton'
-import ShowResults from './ShowResults'
-import Grid from '@material-ui/core/Grid'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import SwitchStyle from './SwitchStyle';
+import React from "react";
+import result from "./data.json";
+import InputField from "./InputField";
+import SearchButton from "./SearchButton";
+import ShowResults from "./ShowResults";
+import Grid from "@material-ui/core/Grid";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import SwitchStyle from "./SwitchStyle";
 
 const theme = createMuiTheme({
-  palette:{
+  palette: {
     primary: {
-      main:'#007BA7',
+      main: "#007BA7"
     },
-    secondary:{
-      main:'#02e2f2'
+    secondary: {
+      main: "#02e2f2"
     }
   },
   typography: {
     fontFamily: [
-      'Nunito',
-      'Roboto',
+      "Nunito",
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
+      "Arial",
+      "sans-serif"
+    ].join(",")
   }
 });
 
 class App extends React.Component {
-  state={
-    inputSphere:0,
-    results:[],
-    darkMode: false,
+  state = {
+    inputSphere: 0,
+    cylinder: 0,
+    addition: 0,
+    results: [],
+    darkMode: false
+  };
+
+  //In order to display all the products we need to use this lifecycle..
+  componentDidMount() {
+    this.getData();
+    this.setState({ inputSphere: 0, cylinder: 0, addition: 0 });
   }
-  findResults=(inputSphere)=>{
-    this.setState({results:result.data})
+
+  getData() {
+    console.log(result.data);
+    this.setState({
+      results: result.data
+    });
   }
-  onSearch=()=>{
-    console.log("to be done")
-  }
-  onChange=(key,value)=>{
-    this.setState({[key]:value})
-  }
-  toggledarkMode=()=>{
+
+  findResults = (inputSphere, cylinder, addition) => {
+    //filter the results that match with the values passed
+    let filteredResults = result.data.filter(result => {
+      if (
+        result.minSphere <= this.state.inputSphere &&
+        this.state.inputSphere <= result.maxSphere &&
+        result.minCylinder <= this.state.cylinder &&
+        this.state.cylinder <= result.maxCylinder &&
+        result.minAddition <= this.state.addition &&
+        this.state.addition <= result.maxAddition
+      ) {
+        return result;
+      }
+    });
+
+    this.setState({ results: filteredResults });
+    console.log("Holiiii", filteredResults);
+  };
+
+  onSearch = e => {
+    //when this function gets activated, it triggers the findResult function in order to filter the results
+
+    //this.onChange(); Do I need this?
+    this.findResults();
+    e.preventDefault();
+  };
+  onChange = (key, value) => {
+    this.setState({ [key]: value });
+    console.log({ [key]: value });
+  };
+  toggledarkMode = () => {
     this.setState({
       darkMode: !this.state.darkMode
-    })
-  }
-  render (){
-    const { darkMode, inputSphere, results } = this.state;
-    return(
+    });
+  };
+  render() {
+    const { darkMode, inputSphere, results, cylinder, addition } = this.state;
+    return (
       <ThemeProvider theme={theme}>
-        <div className={darkMode?"AppDark":"AppLight"}>
-          <Grid className='searchField'>
-          <InputField
-            inputField={inputSphere}
-            name="inputSphere"
-            label="Sphere"
-            onChange={this.onChange}
-          />
-          <SearchButton
-            onSearch={this.onSearch}
-          />
+        <div className={darkMode ? "AppDark" : "AppLight"}>
+          <Grid className="searchField">
+            <InputField
+              inputField={inputSphere}
+              name="inputSphere"
+              label="Sphere"
+              onChange={this.onChange}
+            />
+            <InputField
+              inputField={cylinder}
+              name="cylinder"
+              label="cylinder"
+              onChange={this.onChange}
+            />
+            <InputField
+              inputField={addition}
+              name="addition"
+              label="addition"
+              onChange={this.onChange}
+            />
+            <SearchButton
+              onSearch={this.onSearch}
+              findResults={this.findResults}
+            />
           </Grid>
-          <Grid className={darkMode?'serchResult':'serchResultLight'}>
-          <ShowResults
-            results={results}
-          />
+          <Grid className={darkMode ? "serchResult" : "serchResultLight"}>
+            <ShowResults results={results} />
           </Grid>
           <SwitchStyle
             darkMode={this.state.darkMode}
@@ -74,7 +122,7 @@ class App extends React.Component {
           />
         </div>
       </ThemeProvider>
-    )
+    );
   }
 }
 
